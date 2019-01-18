3bed3cde5ff3c30aac3f70da59717c100cc78e62
warren.veerasingam@gmail.com <warren.veerasingam@gmail.com>
2019-01-18 12:07:10 -0600

    initial code

---
+++ lib/email.go
@@ -0,0 +1,122 @@
+package lib
+
+import (
+	"fmt"
+	"net/smtp"
+	"os"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/ssm"
+	"github.com/matcornic/hermes"
+	gophermail "gopkg.in/jpoehls/gophermail.v0"
+)
+
+const (
+	SUPPORTEMAIL = "support@bittrove.com"
+	APPURL       = "https://bittrove.org"
+	APPNAME      = "BitTrove"
+	APPCOPY      = "Ⓒ 2019 BitTrove - Crafted with ❤ in San Francisco"
+	EMAILSUBJECT = "Wake Up! It's 2015"
+	EMAILYOUTUBE = "https://youtu.be/5nrYOoPeUyA"
+	EMAILWEBSITE = "https://bittrove.org"
+	SEND_OK      = "{ \"message\": \"Message sent successfully\"}"
+	SEND_NOT_OK  = "{ \"message\": \"Unble to send message\"}"
+	IMGHEADER    = "https://s3.us-east-2.amazonaws.com/kepler-images/bittrove/bittrove_with_logo_email.png"
+)
+
+func SendEmail(contact_email string, orgname string, position string, name string) string {
+
+	session := session.Must(session.NewSession())
+	svc := ssm.New(session)
+
+	SMTPPASS := getEmailCredential(svc, "SMTP_PASS")
+
+	SMTPUSER := getEmailCredential(svc, "SMTP_USER")
+
+	SMTPEMAIL := getEmailCredential(svc, "SMTP_EMAIL")
+
+	SMTPPORT := getEmailCredential(svc, "SMTP_PORT")
+
+	emailContent := composeEmail(orgname, position, name)
+	emailHeadFoot := composeEmailFooterHeader(APPURL, APPNAME, APPCOPY)
+
+	emailBody, errBody := emailHeadFoot.GenerateHTML(emailContent)
+	if errBody != nil {
+		fmt.Println(errBody)
+	}
+
+	var msg gophermail.Message
+
+	msg.SetFrom(SUPPORTEMAIL)
+	msg.AddTo(contact_email)
+	msg.SetReplyTo(SUPPORTEMAIL)
+	msg.Subject = orgname + " " + EMAILSUBJECT
+	msg.HTMLBody = emailBody
+
+	auth := smtp.PlainAuth("", SMTPUSER, SMTPPASS, SMTPEMAIL)
+	errEmail := gophermail.SendMail(SMTPPORT, auth, &msg)
+	if errEmail != nil {
+		fmt.Println(errEmail)
+		return SEND_NOT_OK
+	}
+	return SEND_OK
+}
+
+func getEmailCredential(svc *ssm.SSM, val string) string {
+
+	param := &ssm.GetParameterInput{
+		Name:           aws.String(val),
+		WithDecryption: aws.Bool(true),
+	}
+
+	paramVal, err := svc.GetParameter(param)
+	ErrorExit("GetParameters", err)
+
+	smtpInfo := *paramVal.Parameter.Value
+	//fmt.Println(smtpInfo)
+	return smtpInfo
+}
+
+func composeEmail(orgname string, position string, name string) hermes.Email {
+
+	return hermes.Email{
+		Body: hermes.Body{
+			Title: "Hello " + name,
+			Intros: []string{
+				"Excel Spreadsheets and paper files are obsolete!\n",
+				"Today, we would like to introduce you to BitTrove. It’s an intuitive web application that lets organizations keep cumulative records of members, meetings, events, and attendance in one convenient location. Presently, several organizations from different universities use this program. We believe that your organization, " + orgname + " would benefit from BitTrove, too.\n",
+				"Please share this video ad with the officers in your organization: " + EMAILYOUTUBE + "\n",
+				"Register your organization for free at: " + EMAILWEBSITE + "\n",
+				"We look forward to hearing from you.\n",
+			},
+			Outros: []string{
+				"Have more questions? Shoot us an email at " + SUPPORTEMAIL + ".",
+			},
+		},
+	}
+}
+
+func composeEmailFooterHeader(url string, name string, copyright string) hermes.Hermes {
+
+	h := hermes.Hermes{
+		Theme: new(hermes.Default),
+		Product: hermes.Product{
+			// Appears in header & footer of e-mails
+			Name: name,
+			Link: url,
+			// Optional product logo
+			Logo:      IMGHEADER,
+			Copyright: copyright,
+		},
+	}
+
+	return h
+}
+
+func ErrorExit(msg string, e error) {
+	if e != nil {
+		fmt.Fprintf(os.Stderr, "ERROR: %s, %v\n", msg, e)
+		os.Exit(1)
+	}
+}
---
+++ main.go
@@ -0,0 +1,84 @@
+package main
+
+/*
+* Version 0.0.1
+* Compatible on command line - tested on mac
+ */
+
+/*** WORKFLOW ***/
+/*
+* 1- Read command line args - pass csv file
+* 2- Loop through file, collect properties on each line (currently app is hardcoded to accept four columns)
+* 3- Check if each email address is valid
+* 4- Send information to email library
+* 5- Connect to SES using AWS Credentials
+* 6- Sends message using AWS SES
+* 7- Returns OK message if sent was successful
+ */
+
+import (
+	"bufio"
+	"encoding/csv"
+	"fmt"
+	"io"
+	"log"
+	"os"
+
+	"github.com/pborman/getopt"
+	"github.com/warrensbox/bulk-emailer/lib"
+)
+
+func main() {
+
+	helpFlag := getopt.BoolLong("help", 'h', "displays help message")
+	filePath := getopt.StringLong("file", 'f', "", "--file", "Path to csv file")
+
+	getopt.Parse()
+
+	if *helpFlag {
+		usageMessage()
+	} else if *filePath == "" {
+		fmt.Println("You must provide a csv file")
+		usageMessage()
+	}
+
+	csvFile, errorFile := os.Open(*filePath)
+
+	if errorFile != nil {
+		log.Fatal("Unable to open file")
+		os.Exit(1)
+	}
+	reader := csv.NewReader(bufio.NewReader(csvFile))
+
+	fmt.Println("Attempting to send messages!")
+	for {
+		line, error := reader.Read()
+		if error == io.EOF {
+			break
+		} else if error != nil {
+			log.Fatal(error)
+		}
+
+		var orgname = line[0]
+		var position = line[1]
+		var name = line[2]
+		var email = line[3]
+
+		if lib.ValidateFormat(email) == nil && lib.ValidateHost(email) == nil {
+			fmt.Println("Sending email to: " + email)
+			msgInfo := lib.SendEmail(email, orgname, position, name)
+			fmt.Println(msgInfo)
+		} else {
+			fmt.Println("Unable to send message due to invalid format..")
+			fmt.Println("Invalid email: " + email)
+		}
+
+		fmt.Println("---------------------------")
+	}
+}
+
+func usageMessage() {
+	fmt.Print("\n\n")
+	getopt.PrintUsage(os.Stderr)
+	fmt.Println("Pass path to csv file to -f or --file; example ./main -f test.csv")
+}
